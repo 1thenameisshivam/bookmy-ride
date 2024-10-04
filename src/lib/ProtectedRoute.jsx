ec8c4f6516fc929449ac028acbcaf794532bbbf0
1thenameisshivam <1thenameisshivam@gmail.com>
2024-10-05 01:19:08 +0530

    some conflict and bug fixes

--- src/lib/ProtectedRoute.jsx
+++ src/lib/ProtectedRoute.jsx
@@ -1,12 +1,25 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Navigate } from "react-router-dom";
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie"; // Use js-cookie for easy cookie access
+import { useEffect, useState } from "react";
 
 const ProtectedRoute = ({ children }) => {
-  const token = false; // Get token from cookies
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  return token ? children : <Navigate to="/login" />;
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
