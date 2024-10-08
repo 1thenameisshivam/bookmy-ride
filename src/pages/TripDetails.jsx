08bee450136bacc8bd770cac0bb7b4d7d1e494f7
1thenameisshivam <1thenameisshivam@gmail.com>
2024-10-08 20:47:52 +0530

    small bug fix

--- src/pages/TripDetails.jsx
+++ src/pages/TripDetails.jsx
@@ -71,11 +71,11 @@ const TripDetails = () => {
               <div className="grid grid-cols-2 gap-4">
                 <div className="flex items-center">
                   <Calendar className="w-5 h-5 mr-2 text-red-500" />
-                  <span>{tripData.startDate.split("T")[0]}</span>
+                  <span>{tripData?.startDate?.split("T")[0]}</span>
                 </div>
                 <div className="flex items-center">
                   <Clock className="w-5 h-5 mr-2 text-red-500" />
-                  <span>{tripData.duration} days</span>
+                  <span>{tripData?.duration} days</span>
                 </div>
                 <div className="flex items-center">
                   <Users className="w-5 h-5 mr-2 text-red-500" />
@@ -83,7 +83,7 @@ const TripDetails = () => {
                 </div>
                 <div className="flex items-center">
                   <MapPin className="w-5 h-5 mr-2 text-red-500" />
-                  <span>{tripData.destination[0]}</span>
+                  <span>{tripData?.destination}</span>
                 </div>
               </div>
             </div>
