b7c73085f5e12caf05f055c3cbbb637a961a7e97
1thenameisshivam <1thenameisshivam@gmail.com>
2024-10-13 00:11:25 +0530

    busLayout and tripDetail page added

--- src/main.jsx
+++ src/main.jsx
@@ -12,63 +12,73 @@ import Signup from "./pages/Signup.jsx";
 import { Provider } from "react-redux";
 import store from "./lib/Store.js";
 import CreateTrip from "./pages/CreateTrip.jsx";
+import TripDetails from "./pages/TripDetails.jsx";
+import BusLayout from "./pages/BusLayout.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import UnAuthorised from "./pages/UnAuthorised.jsx";
 import AdminAccess from "./lib/AdminAccess.jsx";
 import EditTrip from "./pages/EditTrip.jsx";
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
         path: "/",
-        element: <App />,
-        errorElement: <NotFound />,
-        children: [
-            {
-                path: "/",
-                element: <LandingPage />,
-            },
-            {
-                path: "/trips",
-                element: (
-                    <ProtectedRoute>
-                        <Trips />
-                    </ProtectedRoute>
-                ),
-            },
-            {
-                path: "/login",
-                element: <Login />,
-            },
-            {
-                path: "/signup",
-                element: <Signup />,
-            },
-            {
-                path: "/unauthorise",
-                element: <UnAuthorised />,
-            },
-            {
-                path: "/create/trip",
-                element: (
-                    <AdminAccess>
-                        <CreateTrip />
-                    </AdminAccess>
-                ),
-            },
-            {
-                path: "/edit/trip/:id",
-                element: (
-                    <AdminAccess>
-                        <EditTrip />
-                    </AdminAccess>
-                ),
-            },
-        ],
-    },
+        element: <LandingPage />,
+      },
+      {
+        path: "/trips",
+        element: (
+          <ProtectedRoute>
+            <Trips />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/trips/:id",
+        element: <TripDetails />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/unauthorise",
+        element: <UnAuthorised />,
+      },
+      {
+        path: "/create/trip",
+        element: (
+          <AdminAccess>
+            <CreateTrip />
+          </AdminAccess>
+        ),
+      },
+      {
+        path: "/edit/trip/:id",
+        element: (
+          <AdminAccess>
+            <EditTrip />
+          </AdminAccess>
+        ),
+      },
+      {
+        path: "/trip/:id/book",
+        element: <BusLayout />,
+      },
+    ],
+  },
 ]);
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
 );
